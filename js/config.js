208fa06bb4f7fd5d6d08984247c4a8f40c1ae14b
boris-j-goldberg <32598404+boris-j-goldberg@users.noreply.github.com>
2026-08-11 15:12:58 +0300

    chore(site): bump version to 1.1.17

--- js/config.js
+++ js/config.js
@@ -17,7 +17,7 @@
  * For future cross-page parameters, add them to this object.
  */
 window.PADEL_CONFIG = {
-  version: "1.1.16",
+  version: "1.1.17",
 
   // Add future shared values below as needed.
   // Examples:
